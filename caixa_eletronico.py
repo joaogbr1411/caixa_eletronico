2805d16ced40d9d6ac34903b1266bd4a25bb8953
joaogbr1411 <joaozaccara@gmail.com>
2025-05-24 00:44:30 -0300

    redefinição de senha
    funcionalidade de redefinir senha com confirmação de segurança por email

--- caixa_eletronico.py
+++ caixa_eletronico.py
@@ -3,35 +3,43 @@ import json
 import hashlib
 import smtplib
 from email.message import EmailMessage
+import random
+
 
 path = Path('user.json')
 
 ##verifica cada dicionario em busca do CPF correspondente e da boas vindas ao nome do mesmo
 def login(users):
-    CPF_ver = input('Bem vindo de volta, informe seu CPF: ')
-    Rawpass = input('Digite sua senha: ')
-    Rawpass = Rawpass.encode()
-    senha_ver = hashlib.sha256(Rawpass).hexdigest()
-    for database in users:
-        for CPF in database:
-            if CPF == CPF_ver and senha_ver == database[CPF]['senha']:                
-                print(f'Bem vindo, {database[CPF]["nome_completo"]}!')
-                while True:
-                    action = input(f'Por favor, digite a ação desejada: SACAR, DEPOSITAR ou EXTRATO. (Digite Q para deslogar): ').upper()
-                    if action == 'SACAR':
-                        saque(CPF_ver, users)
-                    elif action == 'DEPOSITAR':    
-                        depositar(CPF_ver, users)
-                    elif action == 'EXTRATO':    
-                        extrato(CPF_ver, users)    
-                    elif action == 'Q':                            
-                        print('Encerrando sistema... Até a próxima!')
-                        break
-                    else:
-                        print('Ação desconhecida.')
-            else:        
-                print('CPF ou senha incorreto. Por favor, verifique os dados inseridos.')
-
+    CPF_ver = input('Bem vindo de volta, informe seu CPF: ')   
+    Rawpass = input('Digite sua senha. Caso tenha esquecido sua senha, digite "REDEFINIR": ')
+    if Rawpass.upper() == 'REDEFINIR':
+        redefinir(users)
+    else:    
+        Rawpass = Rawpass.encode()
+        senha_ver = hashlib.sha256(Rawpass).hexdigest()
+        for database in users:
+            for CPF in database:
+                if CPF == CPF_ver and senha_ver == database[CPF]['senha']:                
+                    print(f'Bem vindo, {database[CPF]["nome_completo"]}!')
+                    while True:
+                        action = input(f'Por favor, digite a ação desejada: SACAR, DEPOSITAR ou EXTRATO. (Digite Q para deslogar): ').upper()
+                        if action == 'SACAR':
+                            saque(CPF_ver, users)
+                        elif action == 'DEPOSITAR':    
+                            depositar(CPF_ver, users)
+                        elif action == 'EXTRATO':    
+                            extrato(CPF_ver, users)    
+                        elif action == 'Q':                            
+                            print('Encerrando sistema... Até a próxima!')
+                            break
+                        else:
+                            print('Ação desconhecida.')
+                else:        
+                    print('CPF ou senha incorreto. Por favor, verifique os dados inseridos.')
+                    recover = input('Gostaria de redefinir sua senha? Digite SIM ou NAO: ')
+                    if recover.upper() == 'SIM':
+                        redefinir(users)
+                    
     return CPF_ver   
 
 def cadastro():
@@ -44,7 +52,7 @@ def cadastro():
                 'idade': input('Digite sua idade: '),
                 'email': input('Digite seu email: '),
                 'senha': input('Crie sua senha: '),
-                'saldo': 0
+                'saldo': int(0)
             }
         
         bytes = usuario['senha'].encode()
@@ -132,6 +140,36 @@ def extrato(CPF_ver, users):
             if CPF == CPF_ver:
                 print(f'Seu saldo é {database[CPF]['saldo']}')
 
+def redefinir(users):
+    CPF_ver = input('Digite seu CPF: ')
+    for database in users:
+        for CPF in database:
+            if CPF_ver == CPF:
+                print('Enviando código de verificação. Isso pode levar alguns instantes...')
+                num = random.randint(100000, 999999)
+                email = database[CPF]['email']
+                msg = EmailMessage()
+                msg['From'] = 'sobek0955@gmail.com'
+                msg['To'] = email
+                msg['Subject'] = 'Recuperação da senha'
+                corpo = f'Recebemos uma solicitação para redefinir sua senha. Para confirmar esta ação, volte ao programa e insira o número de confirmação: {num}'
+                msg.set_content(corpo)
+                with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+                    smtp.starttls() 
+                    smtp.login('sobek0955@gmail.com', 'ltngjmqxmtqbxevc')
+                    smtp.send_message(msg)
+
+                num_ver = int(input('Código de verificação enviado. Por favor, insira o número de confirmação enviado em seu email: '))
+                if num_ver == num:
+                    new_pass = input('Insira sua nova senha: ')
+                    bytes = new_pass.encode()
+                    new_pass = hashlib.sha256(bytes).hexdigest()
+                    database[CPF]['senha'] = new_pass                                                                      
+                    content = json.dumps(users)
+                    path.write_text(content)
+                    print('Sua senha foi redifinida com sucesso.')
+                else:
+                    print('Código de confirmação incorreto. Se você não possui acesso ao email, por favor entre em contato com nosso suporte.')
 
 initial_ver = input('Você já possui uma conta? Digite SIM ou NAO: ').upper() #INICIO DO CÓDIGO
 if initial_ver == 'NAO':
